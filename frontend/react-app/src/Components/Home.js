e5f5fc6b164b6c521b2110691da7f80c25eb8eef
Arinze Nwutobo <tim.arinze@outlook.com>
2023-09-24 01:51:33 +0100

    task

--- frontend/react-app/src/Components/Home.js
+++ frontend/react-app/src/Components/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 import './Home.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+
 
 
 function Home() {
@@ -21,6 +21,7 @@ function Home() {
               classname="me-auto my-2 my-lg-0"
               id="navbarScroll"
             >
+              <div>
               <NavDropdown title="Locations" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Lagos</NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -28,11 +29,13 @@ function Home() {
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action5">Port Harcourt</NavDropdown.Item>
               </NavDropdown>
+              </div>
             </Nav>
             <Nav
               classname="me-auto my-2 my-lg-0"
               id="navbarScroll"
             >
+              <div id="services">
               <NavDropdown title="Services" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Mounting</NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -54,11 +57,42 @@ function Home() {
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action5">Help Moving</NavDropdown.Item>
               </NavDropdown>
+              </div>
             </Nav>
+            <Button variant="outline-success" id="navButton">Login/Sign-up</Button>
+            <Button variant="outline-success" id="navButton">Become a Beedoer</Button>
           </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <section className="hero">
+        <div className="herocontainer">
+          <p className="strongt"><strong>Get help at a reduced cost</strong></p>
+          <div className="separation"></div>
+          <p className="Just">Just Task</p>
+          <input type="text" className="input" placeholder="I need help with ..." />
+          <button className="help">Get help today</button>
+          <div className="otherjobs">
+            <button className="job">Mounting</button>
+            <button className="job">Furniture Assembly</button>
+            <button className="job">Home repair</button>
+            <button className="job">Help Moving</button>
+            <button className="job">Cleaning</button>
+          </div>
+        </div>
+      </section>
+      <section>
+      <div className="album py-5 bg-body-tertiary">
+        <p className="popular"><strong> Popular projects in your area </strong></p>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className="col">
+              <div className="card shadow-sm"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
     </div>
 )}
 
